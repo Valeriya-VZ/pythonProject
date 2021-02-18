1baeff8ea1afb5d6dec370a91a0f46657cf81663
VZ <valeriya.zarozhnaya@gmail.com>
2021-02-18 18:32:04 +0200

    nw

---
+++ selenium/25.3.py
@@ -0,0 +1,21 @@
+import pytest
+@pytest.fixture(autouse=True)
+def testing():
+   pytest.driver = webdriver.Chrome('./chromedriver.exe')
+   # Переходим на страницу авторизации
+   pytest.driver.get('http://petfriends1.herokuapp.com/login')
+
+   yield
+
+   pytest.driver.quit()
+
+
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys('vasya@mail.com')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('12345')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
